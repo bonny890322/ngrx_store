77f75bd0a4ee1a8bfcc3298794736ab2821e519e
dai322 <C107118248@nkust.edu.tw>
2022-11-22 16:11:35 +0800

    feat: 新增 Selector、Action 例子

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,30 @@ export class AppComponent {
     // 分配 loadTodosSuccess 工作，並將得到的結果傳入
     // 此時狀態中的 todosItems 為傳入的內容，且 isLoading 為 false
     this.store.dispatch(loadTodosSuccess({ items: [{ id: 1, text: 'Task 1', done: false }] }));
-  }
 
+    // ---------------------------------------------- Selector ----------------------------------------------
+    this.store
+      .select(selectTodosState) // 指定 Selector
+      .subscribe((data) => console.log(data));
+
+    this.store.select(selectTodoLoading).subscribe((data) => console.log(data));
+
+    this.store.select(selectTodoItemById(1)).subscribe((data) => console.log(data));
+
+    // ---------------------------------------------- Action ----------------------------------------------
+
+    this.store.dispatch(loadTodos()); // Store 分配 (dispatch) 了一個 loadTodos 動作 (Action)
+
+    console.log(loadTodos());
+
+    console.log(loadTodosSuccess({ items: [] }));
+    // { items: [...], type:  "[Todos] Load Todos Success"}
+
+    this.store.dispatch(loadTodosSuccess({ items: [{ id: 1, text: 'Task 1', done: false }] }));
+
+    this.store.dispatch(setTodoDone({ payload: { id: 1, type: 'custom type' } }));
+
+    console.log(setTodoDone({ payload: { id: 1, type: 'custom type' } }));
+  }
 }
+
